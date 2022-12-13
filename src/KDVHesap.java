e73e250697e28fa7998c0e484b184dba658203c4
Onur COŞKUN <onurcoskun85@gmail.com>
2022-12-13 10:13:11 +0300

    KDV Oranı Hesaplama eklendi

--- src/KDVHesap.java
+++ src/KDVHesap.java
@@ -7,13 +7,22 @@ public class KDVHesap {
     }
 
     public static void main(String[] args) {
-        double kdvOran = 0.18;
+        double kdvOran1 = 0.18;
+        double kdvOran2 = 0.08;
         Scanner tara = new Scanner(System.in);
         System.out.println("Lütfen Tutarı Giriniz : ");
         double tutar = tara.nextDouble();
-        double kdvliTutar = tutar * kdvOran;
+        double kdvliTutar;
+        if (tutar>1000){
+            System.out.println("KDV Oranı : " +kdvOran2);
+        }
+        else {
+            System.out.println("KDV Oranı : " +kdvOran1);
+        }
+        kdvliTutar=(tutar>1000)?(tutar * kdvOran2):(tutar * kdvOran1);
         System.out.println("Tutar : " + tutar);
+
         System.out.println("KDV tutarı : " + kdvliTutar);
-        System.out.println("KDV'li Tutar : " + (tutar + kdvliTutar));
+        System.out.println("Sana Oluru Bu : " + (tutar + kdvliTutar));
     }
 }
