2019d66e14db5b2d3d870fe8b3c1f069651dee5f
Onur COŞKUN <onurcoskun85@gmail.com>
2022-12-13 09:39:03 +0300

    KDV Hesabı geldi

--- src/KDVHesap.java
+++ src/KDVHesap.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class KDVHesap {
+package PACKAGE_NAME;
+
+import java.util.Scanner;
+
+public class KDVHesap {
+    public KDVHesap() {
+    }
+
+    public static void main(String[] args) {
+        double kdvOran = 0.18;
+        Scanner tara = new Scanner(System.in);
+        System.out.println("Lütfen Tutarı Giriniz : ");
+        double tutar = tara.nextDouble();
+        double kdvliTutar = tutar * kdvOran;
+        System.out.println("Tutar : " + tutar);
+        System.out.println("KDV tutarı : " + kdvliTutar);
+        System.out.println("KDV'li Tutar : " + (tutar + kdvliTutar));
+    }
 }
