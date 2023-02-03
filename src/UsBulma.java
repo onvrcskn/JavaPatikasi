6037fca961a15c0911501236150f9132db679eb7
Onur COŞKUN <onurcoskun85@gmail.com>
2023-02-03 11:41:26 +0300

    üs bulma eklendi

---
+++ src/UsBulma.java
@@ -0,0 +1,18 @@
+import java.util.Scanner ;
+
+public class UsBulma {
+    public static void main(String[] args) {
+        int n,k,sayi=1;
+        Scanner kuvvet = new Scanner(System.in);
+        System.out.print("taban degerini girin: ");
+        k = kuvvet.nextInt();
+        System.out.print("üs degerini giriniz: ");
+        n = kuvvet.nextInt();
+
+        for (int i=1 ; i<=n ; i++) {
+            sayi *=k;
+        }
+
+        System.out.print("sonuç= " +sayi );
+    }
+}
