b867a55c840729dbd761c173a4b83afbf5b961a7
Onur COŞKUN <onurcoskun85@gmail.com>
2023-02-07 10:15:09 +0300

    Asal sayı bulma eklendi 1-100 arası

---
+++ src/AsalSayiBulma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AsalSayiBulma {
+    public static void main(String[] args) {
+        int sayac = 0;
+        /*System.out.println("bir sayı giriniz : ");
+        Scanner tara = new Scanner(System.in);
+        n = tara.nextInt();*/
+        for (int n=1; n<100;n++) {
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    sayac++;
+                }
+            }
+            if (sayac == 1) {
+                System.out.print(n+" ");
+            }
+            sayac=0;
+        }
+    }
+}
--- src/TersUcgen.java
+++ src/TersUcgen.java
@@ -7,10 +7,10 @@ public class TersUcgen {
         System.out.println("Basamak sayısını giriniz :");
         a = tara.nextInt();
         for (int i = 0; i < a; i++) {
-            for (int k = 0; k <= i; k++) {
+            for (int k = 0; k < i; k++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j < 2 * (a-i)-1; j++) {
+            for (int j = 1; j <= 2 * (a-i)-1; j++) {
                 System.out.print("*");
             }
             System.out.println("");
